dbb434581182acc5808d62eaf2084dd923cf39c5
Achede_HD <hernan.david.hd@gmail.com>
2023-10-21 19:22:57 -0300

    calling AWS credentials from environment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "challenge-terraform-state"
     key    = "challenge/terraform.tfstate"
     region = "us-west-2"
+    access_key = "${var.AWS_ACCESS_KEY_ID}"
+    secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
   }
 }
