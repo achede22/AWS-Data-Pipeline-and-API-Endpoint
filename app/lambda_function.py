cca7125de93a8d55d1792dad2b9011624d813e2b
Achede_HD <hernan.david.hd@gmail.com>
2023-10-22 02:04:00 -0300

    upload lambda_function.py

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,15 +1,45 @@
+import os
 import json
 import boto3
+import time
+
+#### required permissions ####
+# "athena:StartQueryExecution",
+# "athena:GetQueryExecution",
+# "athena:GetQueryResults",
+# "glue:GetTable"
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
+    client = boto3.client('athena')
 
-    bucket = 'hdbucketchallenge'
-    key = 'test1'
-    body = json.dumps(event)  # esto convierte el evento en una cadena JSON
+    # Get the environment variables
+    database_name = os.getenv('DATABASE_NAME')
+    bucket_name = os.getenv('BUCKET_NAME')
 
-    try:
-        s3.put_object(Bucket=bucket, Key=key, Body=body)
-        print(f'Archivo escrito con éxito en {bucket}/{key}')
-    except Exception as e:
-        print(f'Error al escribir en S3: {e}')
+    # Use the provided database name and table name in your query
+    query = "SELECT * FROM passengers limit 20;"  # simple SQL query
+    database = database_name  # AWS Athena database
+    s3_output = "s3://{}/query_results/".format(bucket_name)  # S3 bucket to store query results
+    
+    response = client.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={
+            'Database': database
+        },
+        ResultConfiguration={
+            'OutputLocation': s3_output,
+        }
+    )
+
+    # Observar Resultados
+    query_execution_id = response['QueryExecutionId']
+    queryId = queryStart['QueryExecutionId']
+    time.sleep(15) 
+
+    result_data = client.get_query_results(QueryExecutionId=query_execution_id)
+    # Extract the data from the Athena query results...
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(result_data)
+    }
